cd97eeeca91e8600f60f18d9cf3e071447b2f7e2
Sagar Tiwari <98648990+ReDWoLf1007@users.noreply.github.com>
2023-08-29 16:53:34 +0530

    Create calcy.py
    Simplest program to try for a Python beginner.

---
+++ calcy.py
@@ -0,0 +1,20 @@
+#A simple calculator that calculates 4 operations on two integers
+
+print("Enter 1st Integer: ")
+k = (int)(input())
+print("Enter 2nd Integer: ")
+l = (int)(input())
+
+print("Enter the operation you want to perform (+,-,*,/): ")
+a = input()
+
+if a is + :
+  print("Result: k + l = ", k + l)
+elif a is - :
+  print("Result: k - l = ", k - l)
+elif a is * :
+  print("Result: k * l = ", k * l)
+elif a is / :
+  print("Result: k / l = ", k / l)
+else
+  print("Wrong Choice!!!")
